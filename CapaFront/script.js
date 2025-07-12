bad1e95d3c7fc87987f572ac304a80a469f0f8fb
raulbellidosalas <u202310931@upc.edu.pe>
2025-07-12 08:40:16 -0500

    Update script.js

--- CapaFront/script.js
+++ CapaFront/script.js
@@ -1,5 +1,5 @@
 // API Base URL - Ajusta según tu configuración
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://shapebook-production.up.railway.app//api';
 
 // State management
 let currentUser = null;
